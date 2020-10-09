d8537c42658fd8592c42de83a5181c195a9b5f54
jhg3522 <jhg3522@kookmin.ac.kr>
2020-10-09 22:33:16 +0900

    algorithm 1

---
+++ cp-01.cpp
@@ -0,0 +1,37 @@
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> max_mul(int i, int n) {
+	int mul=1;
+	vector<int> a;
+	vector<int> ans(2);
+	while (n != 0) {
+		a.push_back(n%i);
+		n /= i;
+	}
+	for (int j = 0; j < a.size(); j++) {
+		if (a[j] != 0) {
+			mul *= a[j];
+		}
+	}
+	ans[0]=i;
+	ans[1]=mul;
+	return ans;
+}
+
+int main() {
+	int n,i;
+	int arr[2] = { 0,1 };
+	cin >> n;
+	for (int i = 2; i <= n; i++) {
+		if (arr[1] <= max_mul(i, n)[1]) {
+			arr[1] = max_mul(i, n)[1];
+			arr[0] = max_mul(i, n)[0];
+		}
+	}
+	cout << "[" << arr[0] << "," <<  arr[1] << "]";
+
+}
