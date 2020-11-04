41ce08763d49a404948c29831cd492b4a9cc036e
jhg3522 <jhg3522@kookmin.ac.kr>
2020-11-04 23:33:20 +0900

    Update AA36.cpp

--- AA36.cpp
+++ AA36.cpp
@@ -1,23 +1,25 @@
 #include<iostream>
 using namespace std;
+//»ðÀÔÁ¤·Ä
 
 int main() {
-	int n, i, j, tmp;
 	int arr[101];
+	int n,i, j, tmp;
 	cin >> n;
 	for (i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n - i - 1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				tmp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = tmp;
-			}
+	for (i = 1; i < n; i++) {
+		tmp = arr[i];
+		for (j = i - 1; j >= 0; j--) {
+			if (arr[j] > tmp)
+				arr[j + 1] = arr[j];
+			else
+				break;
 		}
+		arr[j + 1] = tmp;
 	}
-	for (i = 0; i <n; i++) {
-		cout << arr[i] << " ";
+	for (i = 0; i < n; i++) {
+		cout <<arr[i]<<" ";
 	}
 }
