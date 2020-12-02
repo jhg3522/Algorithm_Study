a9e72af05b227661274e70cec887863652b6a4b0
jhg3522 <jhg3522@kookmin.ac.kr>
2020-12-03 01:05:41 +0900

    chapter2

---
+++ python/AA/example/variable_input.py
@@ -0,0 +1,21 @@
+'''
+변수입력과 연산자
+'''
+# a=input("숫자를 입력하세요 :")
+# print(a)
+
+# a, b = input("숫자를 입력하세요 : ").split()띄어쓰기로 입력 구분
+#print(a+b) #ab a,b가 문자열이기 때문에 붙어주기만 함
+
+'''
+map을 이용한 int로 mapping
+a, b = map(int,input("숫자를 입력하세요 : ").split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) # 몫
+print(a%b)  # 나머지
+print(a**b) # 거듭제곱 2,3 넣으면 2의 3제곱2
+'''
+
